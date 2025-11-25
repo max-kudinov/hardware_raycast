b2bb0d64642daa46a6218f9c38df2ad48ed7bd70
Max Kudinov <max.kudinov@proton.me>
2025-11-26 02:40:01 +0300

    Add FSM logic

--- rtl/ray_height_calc.sv
+++ rtl/ray_height_calc.sv
@@ -28,4 +28,60 @@ module ray_height_calc #(
 
 localparam RAY_STEP = int'(2.0 / FRAME_WIDTH * (2**W_FRAC));
 
+// ----------------------------------------------------------------------------
+// Local signals declaration
+// ----------------------------------------------------------------------------
+
+logic inv_done;
+logic dda_done;
+logic wall_dist_zero;
+
+typedef enum {
+    ST_IDLE,
+    ST_CALC_RAY_X,
+    ST_CALC_RAY_DIR,
+    ST_CALC_DELTA_DIST_X,
+    ST_CALC_DELTA_DIST_Y,
+    ST_CALC_PERP_DIST,
+    ST_CALC_SIDE_DIST,
+    ST_RUN_DDA,
+    ST_CALC_WALL_DIST,
+    ST_INV_WALL_DIST,
+    ST_CALC_LINE_HEIGHT,
+    ST_RES_OUT
+} state_t;
+
+state_t state, next_state;
+
+// ----------------------------------------------------------------------------
+// FSM
+// ----------------------------------------------------------------------------
+
+always_comb begin
+    next_state = state;
+
+    case (state)
+        ST_IDLE:              if (start_i)        next_state = ST_CALC_RAY_X;
+        ST_CALC_RAY_X:                            next_state = ST_CALC_RAY_DIR;
+        ST_CALC_RAY_DIR:                          next_state = ST_CALC_DELTA_DIST_X;
+        ST_CALC_DELTA_DIST_X: if (inv_done)       next_state = ST_CALC_DELTA_DIST_Y;
+        ST_CALC_DELTA_DIST_Y: if (inv_done)       next_state = ST_CALC_PERP_DIST;
+        ST_CALC_PERP_DIST:                        next_state = ST_CALC_SIDE_DIST;
+        ST_CALC_SIDE_DIST:                        next_state = ST_RUN_DDA;
+        ST_RUN_DDA:           if (dda_done)       next_state = ST_CALC_WALL_DIST;
+        ST_CALC_WALL_DIST:    if (wall_dist_zero) next_state = ST_RES_OUT;
+                              else                next_state = ST_INV_WALL_DIST;
+        ST_INV_WALL_DIST:     if (inv_done)       next_state = ST_CALC_LINE_HEIGHT;
+        ST_CALC_LINE_HEIGHT:                      next_state = ST_RES_OUT;
+        ST_RES_OUT:           if (start_i)        next_state = ST_CALC_RAY_X;
+                              else                next_state = ST_IDLE;
+    endcase
+end
+
+always_ff @(posedge clk)
+    if (rst)
+        state <= ST_IDLE;
+    else
+        state <= next_state;
+
 endmodule
