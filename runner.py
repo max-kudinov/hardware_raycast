5778ac54a7f20079c4559b27c2a7e759e57330b4
Maxim Kudinov <max.kudinov@proton.me>
2025-11-27 22:30:21 +0300

    Add top level parameter values to runner

--- runner.py
+++ runner.py
@@ -5,6 +5,13 @@ from cocotb_tools.runner import get_runner
 
 def run_tb():
     sim = "verilator"
+
+    parameters = {
+        "W_INT": 5,
+        "W_FRAC": 9,
+        "N_ITER": 5
+    }
+
     proj_path = Path(__file__).resolve().parent
     sources = [proj_path / "rtl/newton_inv.sv"]
 
@@ -12,12 +19,14 @@ def run_tb():
 
     runner.build(
         sources=sources,
-        hdl_toplevel="newton_inv"
+        hdl_toplevel="newton_inv",
+        parameters=parameters
     )
 
     runner.test(
         hdl_toplevel="newton_inv",
-        test_module="tb"
+        test_module="tb",
+        parameters=parameters
     )
 
 
