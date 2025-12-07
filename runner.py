95012142f1d5652192566572dfd0df8096a9f71e
Max Kudinov <max.kudinov@proton.me>
2025-12-08 02:17:29 +0300

    Add waves and new sources

--- runner.py
+++ runner.py
@@ -5,12 +5,16 @@ from cocotb_tools.runner import get_runner
 
 def run_tb():
     sim = "verilator"
+    waves = True
 
     parameters = {
     }
 
     proj_path = Path(__file__).resolve().parent
     sources = [proj_path / "rtl/newton_inv.sv"]
+    sources += [proj_path / "rtl/dda.sv"]
+    sources += [proj_path / "rtl/line_height_calc.sv"]
+    sources += [proj_path / "rtl/raycast_top.sv"]
     includes = [proj_path / "rtl/include"]
 
     runner = get_runner(sim)
@@ -18,14 +22,18 @@ def run_tb():
     runner.build(
         sources=sources,
         includes=includes,
-        hdl_toplevel="newton_inv",
-        parameters=parameters
+        hdl_toplevel="raycast_top",
+        parameters=parameters,
+        waves=waves,
+        build_args=["--timing", "--trace", "--trace-fst", "--trace-structs"]
+        # build_args=["--timing"]
     )
 
     runner.test(
-        hdl_toplevel="newton_inv",
+        hdl_toplevel="raycast_top",
         test_module="tb",
-        parameters=parameters
+        parameters=parameters,
+        waves=waves
     )
 
 
