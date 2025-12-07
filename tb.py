2edb6c07f2ec777304b6361ecb2179010ded9bec
Max Kudinov <max.kudinov@proton.me>
2025-12-08 02:20:19 +0300

    Add fixed point software model and implement self checking

--- tb.py
+++ tb.py
@@ -1,8 +1,10 @@
 import math
+from fpbinary import FpBinary, FpBinarySwitchable
 import pygame as pg
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
+
 
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
@@ -10,8 +12,16 @@ FRAME_HEIGHT = 480
 MAP_WIDTH = 20
 MAP_HEIGHT = 20
 
+W_INT = int(cocotb.packages.fixedpoint.W_INT.value)
+W_FRAC = int(cocotb.packages.fixedpoint.W_FRAC.value)
+
+FP_MODE = True
+
 pg.init()
-font = pg.freetype.Font("/usr/share/fonts/TTF/JetBrainsMonoNLNerdFont-Regular.ttf", 24)
+font = pg.freetype.Font(
+    "/usr/share/fonts/TTF/JetBrainsMonoNLNerdFont-Regular.ttf",
+    24
+)
 surface = pg.display.set_mode((FRAME_WIDTH, FRAME_HEIGHT))
 
 game_map = [
@@ -42,27 +52,25 @@ pos_x = 10
 pos_y = 10
 
 # Camera direction
-dir_x = -1
-dir_y = 0
+dir_x = 0.94
+dir_y = -0.33
 
 # Camera plane vector
-plane_x = 0
-plane_y = 0.66
+plane_x = -0.22
+plane_y = -0.62
 
 time = 0
 old_time = 0
 
+ray_step = int(2.0 / FRAME_WIDTH * (2**(W_FRAC)))
+
 
 def float_to_fixp(num_float):
-    frac_bits = int(cocotb.packages.fixedpoint.W_FRAC.value)
-
-    return int(num_float * (2 ** frac_bits))
+    return int(num_float * (2 ** W_FRAC))
 
 
 def fixp_to_float(num_fixp):
-    frac_bits = int(cocotb.packages.fixedpoint.W_FRAC.value)
-
-    return float(num_fixp) / (2 ** frac_bits)
+    return float(num_fixp) / (2 ** W_FRAC)
 
 
 async def newton_inv(dut, recip):
@@ -79,17 +87,106 @@ async def newton_inv(dut, recip):
     return fixp_to_float(int(dut.num_o.value))
 
 
+def inv_model(num):
+    cnt = 0
+    approx = fixp_init(val=1)
+
+    while num > 1:
+        num = num / 2
+        cnt += 1
+
+    for _ in range(5):
+        approx.value = approx * (2 - num * approx)
+
+    approx.value = approx / (2**cnt)
+    return approx
+
+
+def fixp_init(int_bits=W_INT, frac_bits=W_FRAC, val=0, signed=False):
+    fixp = FpBinary(
+        int_bits=int_bits,
+        frac_bits=frac_bits,
+        signed=signed,
+        value=val
+    )
+
+    return FpBinarySwitchable(
+        fp_mode=FP_MODE,
+        fp_value=fixp,
+        float_value=val
+    )
+
+
+def fixp_max_val():
+    return 2**W_INT - 1 + ((2**W_FRAC - 1) / (2**W_FRAC))
+
+
+fixp_frame_height = fixp_init(val=FRAME_HEIGHT, int_bits=10)
+ray_dir_x = fixp_init(signed=True)
+ray_dir_y = fixp_init(signed=True)
+delta_dist_x = fixp_init()
+delta_dist_y = fixp_init()
+perp_wall_dist = fixp_init()
+inv_perp_wall_dist = fixp_init()
+side_dist_x = fixp_init()
+side_dist_y = fixp_init()
+
+
 async def line_height_calc(dut, x):
-    ray_step = float_to_fixp(2 / FRAME_WIDTH)
-    ray = x * ray_step - 1
-    ray_x = fixp_to_float(ray)
+    await RisingEdge(dut.clk)
+    dut.start_i.value = 1
+    dut.px_x_i.value = x
+    dut.pos_x_i.value = float_to_fixp(pos_x)
+    dut.pos_y_i.value = float_to_fixp(pos_y)
+    dut.dir_x_i.value = float_to_fixp(dir_x)
+    dut.dir_y_i.value = float_to_fixp(dir_y)
+    dut.plane_x_i.value = float_to_fixp(plane_x)
+    dut.plane_y_i.value = float_to_fixp(plane_y)
+
+    await RisingEdge(dut.clk)
+    dut.start_i.value = 0
+
+    while not dut.done_o.value:
+        await RisingEdge(dut.clk)
+
+    if dut.ray_hit_side_o.value:
+        line_color = (128, 128, 128)
+    else:
+        line_color = (255, 255, 255)
+
+    return (int(dut.height_o.value), line_color)
+
+
+def line_height_calc_model(x):
+    global ray_dir_x
+    global ray_dir_y
+    global perp_wall_dist
+    global side_dist_x
+    global side_dist_y
+    global delta_dist_x
+    global delta_dist_y
+
+    ray_x = fixp_init(val=(x * ray_step / 2**W_FRAC - 1), signed=True)
+
     ray_dir_x = dir_x + plane_x * ray_x
     ray_dir_y = dir_y + plane_y * ray_x
 
-    delta_dist_x = 1e30 if float_to_fixp(ray_dir_x) == 0 else await newton_inv(dut, abs(ray_dir_x))
-    delta_dist_y = 1e30 if float_to_fixp(ray_dir_y) == 0 else await newton_inv(dut, abs(ray_dir_y))
+    if ray_dir_x == 0:
+        delta_dist_x.value = fixp_max_val()
+    else:
+        if ray_dir_x > 0:
+            delta_dist_x = inv_model(ray_dir_x)
+        else:
+            delta_dist_x = inv_model(-ray_dir_x)
 
-    perp_wall_dist = 0
+    if ray_dir_y == 0:
+        delta_dist_y.value = fixp_max_val()
+    else:
+        if ray_dir_y > 0:
+            delta_dist_y = inv_model(ray_dir_y)
+        else:
+            delta_dist_y = inv_model(-ray_dir_y)
+
     map_x = int(pos_x)
     map_y = int(pos_y)
 
@@ -129,17 +226,25 @@ async def line_height_calc(dut, x):
         perp_wall_dist = side_dist_y - delta_dist_y
         line_color = (128, 128, 128)
 
-    if float_to_fixp(perp_wall_dist) == 0:
-        return (FRAME_HEIGHT, line_color)
+    if perp_wall_dist == 0:
+        return (int(fixp_frame_height), line_color)
     else:
-        return (int(FRAME_HEIGHT * await newton_inv(dut, perp_wall_dist)), line_color)
+        inv_perp_wall_dist = inv_model(perp_wall_dist)
+        return (int(fixp_frame_height * inv_perp_wall_dist), line_color)
 
 
 async def game(dut):
     """Game loop"""
 
     for x in range(FRAME_WIDTH):
+        line_height_model, line_color_model = line_height_calc_model(x)
         line_height, line_color = await line_height_calc(dut, x)
+
+        if (line_height_model != line_height):
+            print(f"pixel {x}")
+            print(f"Expected {line_height_model}, got: {line_height}")
+            exit(0)
+
         start_pos = FRAME_HEIGHT // 2 - line_height // 2
 
         if start_pos < 0:
@@ -168,11 +273,11 @@ def controls():
     move_speed = 0.3 * 4.0
     rot_speed = 0.3 * 2.0
     fps = 1 / frame_time
-    font.render_to(surface, (20, 20), str(fps), (255, 255, 255))
-
-    font.render_to(surface, (20, 110), f"pos_x: {pos_x}", (255, 255, 255))
-    font.render_to(surface, (20, 140), f"pos_y: {pos_y}", (255, 255, 255))
-    font.render_to(surface, (20, 170), f"frame_time: {frame_time}", (255, 255, 255))
+    font.render_to(surface, (20, 20), f"FPS: {str(fps)}", (0, 255, 0))
+    font.render_to(surface, (20, 50), f"plane_x: {plane_x}", (0, 255, 0))
+    font.render_to(surface, (20, 80), f"plane_y: {plane_y}", (0, 255, 0))
+    font.render_to(surface, (20, 110), f"dir_x: {dir_x}", (0, 255, 0))
+    font.render_to(surface, (20, 140), f"dir_y: {dir_y}", (0, 255, 0))
 
     for event in pg.event.get():
         if event.type == pg.KEYDOWN:
@@ -223,7 +328,7 @@ async def dut_reset(dut):
 
 @cocotb.test()
 async def run_raycast(dut):
-    Clock(dut.clk, 1, "ns").start()
+    # Clock(dut.clk, 1, "ns").start()
     cocotb.start_soon(dut_reset(dut))
     while True:
         await game(dut)
