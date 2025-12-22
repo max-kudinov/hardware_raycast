bd4359a3135cfcfe7236143dfba90ad94ad75d03
Max Kudinov <max.kudinov@proton.me>
2025-12-23 01:16:28 +0300

    Add frame buffer

--- rtl/render.sv
+++ rtl/render.sv
@@ -6,14 +6,17 @@ module render
     import fixedpoint::W_INT;
     import fixedpoint::W_FRAC;
 #(
-    parameter W_X_POS = 8,
-    parameter W_Y_POS = 8
+    parameter FRAME_WIDTH = 640,
+    parameter W_HEIGHT    = 8,
+    parameter W_X_POS     = 8,
+    parameter W_Y_POS     = 8
 ) (
     input  var logic                          clk,
     input  var logic                          rst,
 
     input  var logic        [W_X_POS-1:0]     px_x_i,
     input  var logic        [W_Y_POS-1:0]     px_y_i,
+    input  var logic                          in_range_i,
 
     // Camera coordinates
     input  var logic        [W_INT-1:-W_FRAC] pos_x_i,
@@ -32,6 +35,26 @@ module render
 
     output var logic        [23:0]            color_o
 );
+
+localparam BUF_PIXELS = FRAME_WIDTH;
+localparam W_BUF_DATA = W_HEIGHT + 1;
+localparam W_BUF_ADDR = $clog2(BUF_PIXELS);
+
+logic [W_BUF_DATA-1:0] frame_buffer [BUF_PIXELS];
+logic                  buf_write;
+logic                  buf_read;
+logic [W_BUF_ADDR-1:0] buf_addr;
+logic [W_BUF_DATA-1:0] buf_data_in;
+logic [W_BUF_DATA-1:0] buf_data_out;
+
+// Single-port block RAM
+always_ff @(posedge clk) begin
+    if (buf_write) begin
+        frame_buffer[buf_addr] <= buf_data_in;
+    end else if (buf_read) begin
+        buf_data_out <= frame_buffer[buf_addr];
+    end
+end
 
 endmodule
 
