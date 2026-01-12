bd33fbc28032860f10fdfbed4414d52998267435
Maxim Kudinov <max.kudinov@proton.me>
2026-01-12 23:51:56 +0300

    Add FSMs

--- rtl/rotation.sv
+++ rtl/rotation.sv
@@ -41,6 +41,101 @@ localparam sfixp_t SIN_ANGLE     = fixedpoint::real_to_sfixp($sin(ROTATION_SPEED
 localparam sfixp_t COS_NEG_ANGLE = fixedpoint::real_to_sfixp($cos(-ROTATION_SPEED));
 localparam sfixp_t SIN_NEG_ANGLE = fixedpoint::real_to_sfixp($sin(-ROTATION_SPEED));
 
+// ----------------------------------------------------------------------------
+// Local types declaration
+// ----------------------------------------------------------------------------
+
+typedef enum {
+    ST_UPDATE_DIR,
+    ST_UPDATE_PLANE
+} vect_state_t;
+
+typedef enum {
+    ST_CALC_IDLE,
+    ST_X_MULT_COS,
+    ST_X_MULT_SIN,
+    ST_X_SUB,
+    ST_Y_MULT_SIN,
+    ST_Y_MULT_COS,
+    ST_Y_ADD
+} calc_state_t;
+
+// ----------------------------------------------------------------------------
+// Local signals declaration
+// ----------------------------------------------------------------------------
+
+logic update_done;
+logic update_enable;
+logic signed [W_INT-1:-W_FRAC] cur_cos;
+logic signed [W_INT-1:-W_FRAC] cur_sin;
+
+vect_state_t vect_state;
+vect_state_t vect_next_state;
+
+calc_state_t calc_state;
+calc_state_t calc_next_state;
+
+// ----------------------------------------------------------------------------
+// FSMs
+// ----------------------------------------------------------------------------
+
+always_ff @(posedge clk)
+    if (rst)
+        vect_state <= ST_UPDATE_DIR;
+    else
+        vect_state <= vect_next_state;
+
+always_comb begin
+    vect_next_state = vect_state;
+
+    // case (vect_state)
+    //     ST_UPDATE_DIR: if (/* DONE*/)
+    //     ST_UPDATE_PLANE: if (/* DONE */)
+    // endcase
+end
+
+always_ff @(posedge clk)
+    if (rst)
+        calc_state <= ST_CALC_IDLE;
+    else
+        calc_state <= calc_next_state;
+
+always_comb begin
+    calc_next_state = calc_state;
+
+    unique case (calc_state)
+        ST_CALC_IDLE:  if (update_start_i) calc_next_state = ST_X_MULT_COS;
+        ST_X_MULT_COS:                     calc_next_state = ST_X_MULT_SIN;
+        ST_X_MULT_SIN:                     calc_next_state = ST_X_SUB;
+        ST_X_SUB:                          calc_next_state = ST_Y_MULT_SIN;
+        ST_Y_MULT_SIN:                     calc_next_state = ST_Y_MULT_COS;
+        ST_Y_ADD:      if (update_done)    calc_next_state = ST_CALC_IDLE;
+                       else                calc_next_state = ST_X_MULT_COS;
+    endcase
+end
+
+// ----------------------------------------------------------------------------
+// New field of view calculation
+// ----------------------------------------------------------------------------
+
+// Rotate only when 1 key is pressed
+assign update_enable = key_rotate_left_i ^ key_rotate_right_i;
+
+// To rotate we have to multiply vector components by rotation matrix
+// The formula is:
+// x_new = x * cos(rot_angle) - y * sin(rot_angle)
+// y_new = x * sin(rot_angle) + y * cos(rot_angle)
+
+always_ff @(posedge clk)
+    if (update_start_i)
+        if (key_rotate_left_i) begin // Counter clockwise
+            cur_cos <= COS_ANGLE;
+            cur_sin <= SIN_ANGLE;
+        end else begin               // Clockwise
+            cur_cos <= COS_NEG_ANGLE;
+            cur_sin <= SIN_NEG_ANGLE;
+        end
+
 always_ff @(posedge clk)
     if (rst) begin
         dir_x_o   <= fixedpoint::real_to_sfixp(START_DIR_X);
