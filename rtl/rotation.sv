7a2ee0846141bedb33bf22926ee23cbb7f82a425
Maxim Kudinov <max.kudinov@proton.me>
2026-01-13 00:24:17 +0300

    Generate output

--- rtl/rotation.sv
+++ rtl/rotation.sv
@@ -64,10 +64,20 @@ typedef enum {
 // Local signals declaration
 // ----------------------------------------------------------------------------
 
+logic signed [W_INT-1:-W_FRAC] dir_x_next;
+logic signed [W_INT-1:-W_FRAC] dir_y_next;
+
+logic signed [W_INT-1:-W_FRAC] plane_x_next;
+logic signed [W_INT-1:-W_FRAC] plane_y_next;
+
 logic update_done;
 logic update_enable;
 logic signed [W_INT-1:-W_FRAC] cur_cos;
 logic signed [W_INT-1:-W_FRAC] cur_sin;
+
+logic signed [W_INT-1:-W_FRAC] x_prev;
+logic signed [W_INT-1:-W_FRAC] y_prev;
+logic signed [W_INT-1:-W_FRAC] comp_new;
 
 logic vect_done;
 
@@ -90,7 +100,7 @@ always_ff @(posedge clk)
 always_comb begin
     vect_next_state = vect_state;
 
-    case (vect_state)
+    unique case (vect_state)
         ST_UPDATE_DIR:   if (vect_done) vect_next_state = ST_UPDATE_PLANE;
         ST_UPDATE_PLANE: if (vect_done) vect_next_state = ST_UPDATE_DIR;
     endcase
@@ -141,12 +151,49 @@ always_ff @(posedge clk)
         end
 
 always_ff @(posedge clk)
+    if (update_start_i || vect_done)
+        if (vect_next_state == ST_UPDATE_DIR) begin
+            x_prev <= dir_x_o;
+            y_prev <= dir_y_o;
+        end else begin
+            x_prev <= plane_x_o;
+            y_prev <= plane_y_o;
+        end
+
+// ----------------------------------------------------------------------------
+// Output
+// ----------------------------------------------------------------------------
+
+always_comb begin
+    dir_x_next   = dir_x_o;
+    dir_y_next   = dir_y_o;
+
+    plane_x_next = plane_x_o;
+    plane_y_next = plane_y_o;
+
+    if (update_enable) begin
+        unique0 case ({vect_state, calc_state})
+            { ST_UPDATE_DIR,   ST_X_SUB }: dir_x_next   = comp_new;
+            { ST_UPDATE_DIR,   ST_Y_ADD }: dir_y_next   = comp_new;
+            { ST_UPDATE_PLANE, ST_X_SUB }: plane_x_next = comp_new;
+            { ST_UPDATE_PLANE, ST_Y_ADD }: plane_y_next = comp_new;
+        endcase
+    end
+end
+
+always_ff @(posedge clk)
     if (rst) begin
         dir_x_o   <= fixedpoint::real_to_sfixp(START_DIR_X);
         dir_y_o   <= fixedpoint::real_to_sfixp(START_DIR_Y);
 
         plane_x_o <= fixedpoint::real_to_sfixp(START_PLANE_X);
         plane_y_o <= fixedpoint::real_to_sfixp(START_PLANE_Y);
+    end else begin
+        dir_x_o   <= dir_x_next;
+        dir_y_o   <= dir_y_next;
+
+        plane_x_o <= plane_x_next;
+        plane_y_o <= plane_y_next;
     end
 
 endmodule
