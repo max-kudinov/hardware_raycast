ead4de9e044dd5eb70967cc70b23a13665abff30
Maxim Kudinov <max.kudinov@proton.me>
2026-01-13 00:56:58 +0300

    Finish vector rotation calculation

--- rtl/rotation.sv
+++ rtl/rotation.sv
@@ -70,22 +70,27 @@ logic signed [W_INT-1:-W_FRAC] dir_y_next;
 logic signed [W_INT-1:-W_FRAC] plane_x_next;
 logic signed [W_INT-1:-W_FRAC] plane_y_next;
 
-logic update_done;
-logic update_enable;
 logic signed [W_INT-1:-W_FRAC] cur_cos;
 logic signed [W_INT-1:-W_FRAC] cur_sin;
+
+logic signed [W_INT-1:-W_FRAC] cos_mult_next;
+logic signed [W_INT-1:-W_FRAC] sin_mult_next;
+logic signed [W_INT-1:-W_FRAC] cos_mult_ff;
+logic signed [W_INT-1:-W_FRAC] sin_mult_ff;
 
 logic signed [W_INT-1:-W_FRAC] x_prev;
 logic signed [W_INT-1:-W_FRAC] y_prev;
 logic signed [W_INT-1:-W_FRAC] comp_new;
 
-logic vect_done;
+logic                          update_done;
+logic                          update_enable;
+logic                          vect_done;
 
-vect_state_t vect_state;
-vect_state_t vect_next_state;
+vect_state_t                   vect_state;
+vect_state_t                   vect_next_state;
 
-calc_state_t calc_state;
-calc_state_t calc_next_state;
+calc_state_t                   calc_state;
+calc_state_t                   calc_next_state;
 
 // ----------------------------------------------------------------------------
 // FSMs
@@ -121,6 +126,7 @@ always_comb begin
         ST_X_MULT_SIN:                     calc_next_state = ST_X_SUB;
         ST_X_SUB:                          calc_next_state = ST_Y_MULT_SIN;
         ST_Y_MULT_SIN:                     calc_next_state = ST_Y_MULT_COS;
+        ST_Y_MULT_COS:                     calc_next_state = ST_Y_ADD;
         ST_Y_ADD:      if (update_done)    calc_next_state = ST_CALC_IDLE;
                        else                calc_next_state = ST_X_MULT_COS;
     endcase
@@ -160,6 +166,28 @@ always_ff @(posedge clk)
             y_prev <= plane_y_o;
         end
 
+always_comb begin
+    cos_mult_next = cos_mult_ff;
+    sin_mult_next = sin_mult_ff;
+
+    unique0 case (calc_state)
+        ST_X_MULT_COS: cos_mult_next = fixedpoint::signed_mult(x_prev, cur_cos);
+        ST_X_MULT_SIN: sin_mult_next = fixedpoint::signed_mult(x_prev, cur_sin);
+        ST_Y_MULT_SIN: sin_mult_next = fixedpoint::signed_mult(y_prev, cur_sin);
+        ST_Y_MULT_COS: cos_mult_next = fixedpoint::signed_mult(y_prev, cur_cos);
+    endcase
+
+    if (calc_state == ST_X_SUB)
+        comp_new = cos_mult_ff - sin_mult_ff;
+    else
+        comp_new = sin_mult_ff + cos_mult_ff;
+end
+
+always_ff @(posedge clk) begin
+    cos_mult_ff <= cos_mult_next;
+    sin_mult_ff <= sin_mult_next;
+end
+
 // ----------------------------------------------------------------------------
 // Output
 // ----------------------------------------------------------------------------
@@ -172,7 +200,7 @@ always_comb begin
     plane_y_next = plane_y_o;
 
     if (update_enable) begin
-        unique0 case ({vect_state, calc_state})
+        unique0 case ({ vect_state, calc_state })
             { ST_UPDATE_DIR,   ST_X_SUB }: dir_x_next   = comp_new;
             { ST_UPDATE_DIR,   ST_Y_ADD }: dir_y_next   = comp_new;
             { ST_UPDATE_PLANE, ST_X_SUB }: plane_x_next = comp_new;
