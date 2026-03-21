4c9c76a84028e1f40e6a6f3375b1cea1330f532a
Maxim Kudinov <max.kudinov@proton.me>
2026-03-21 14:45:11 +0300

    Create memfiles from images

--- texture_memfile.py
+++ texture_memfile.py
@@ -1,40 +1,63 @@
+import os
 from PIL import Image
 
-texture = Image.open("textures/wall_vines3.png", mode="r").convert("RGB")
+TEX_SIDE = 32
+RECODE_LUT_LEN = 15
 
-colors = list()
-color_lut = dict()
-texture_recoded = list()
+tex_images = [
+    "wall_vines3.png",
+    "volcanic_wall0.png",
+    "lair1.png",
+    "relief3.png",
+    "crystal_wall10.png",
+    "brick_gray2.png",
+    "lava3.png",
+]
 
-for y in range(32):
-    for x in range(32):
-        color = texture.getpixel((x, y))
+try:
+    os.remove("memfiles/recode_lut.mem")
+    os.remove("memfiles/textures.mem")
+except OSError:
+    pass
 
-        if color not in colors:
-            colors.append(color)
+textures = list()
 
-for addr, col in enumerate(colors):
-    r, g, b = col
-    print(f"Address: {addr}, color: 8'd{r}, 8'd{g}, 8'd{b}")
-    color_lut[col] = addr
+for image in tex_images:
+    textures.append(Image.open(f"textures/{image}").convert("RGB"))
 
-print()
+for texture in textures:
+    colors = list()
+    color_lut = dict()
+    texture_recoded = list()
 
-for y in range(32):
-    recoded_row = list()
-    for x in range(32):
-        color = texture.getpixel((x, y))
-        recoded_row.append(color_lut[color])
+    for y in range(TEX_SIDE):
+        for x in range(TEX_SIDE):
+            color = texture.getpixel((x, y))
 
-    texture_recoded.append(recoded_row)
+            if color not in colors:
+                colors.append(color)
 
+    with open("memfiles/recode_lut.mem", "a") as f:
+        for addr, col in enumerate(colors):
+            r, g, b = col
+            f.write(f"{(r << 16) + (g << 8) + b:x}\n")
+            color_lut[col] = addr
 
-for y in range(32):
-    print("{ ", end="")
-    for x in range(31, -1, -1):
-        print(f"4'd{texture_recoded[y][x]}", end="")
-        if x != 0:
-            print(", ", end="")
+        for _ in range(RECODE_LUT_LEN - len(colors)):
+            f.write("0\n")
 
-    print(" }", end="")
-    print()
+    for y in range(TEX_SIDE):
+        recoded_row = list()
+        for x in range(TEX_SIDE):
+            color = texture.getpixel((x, y))
+            recoded_row.append(color_lut[color])
+
+        texture_recoded.append(recoded_row)
+
+    with open("memfiles/textures.mem", "a") as f:
+        for y in range(TEX_SIDE):
+            for x in range(TEX_SIDE):
+                f.write(f"{texture_recoded[y][x]:x}")
+                if x != TEX_SIDE-1:
+                    f.write(" ")
+            f.write("\n")
