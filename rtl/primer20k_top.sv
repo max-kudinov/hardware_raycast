0bf41fd82543273a5f4b8accbb31aaeb344ac72f
Maxim Kudinov <max.kudinov@proton.me>
2026-04-17 19:04:11 +0300

    Display output refactor

--- rtl/primer20k_top.sv
+++ rtl/primer20k_top.sv
@@ -1,3 +1,5 @@
+`include "dvi_pkg.svh"
+
 `default_nettype none
 
 module primer20k_top #(
@@ -8,13 +10,15 @@ module primer20k_top #(
     input  var logic       clk,
 `endif
     input  var logic       rst_n,
-    input  var logic [5:0] keys_inv_i,
+    input  var logic [5:0] keys_inv_i
 `ifdef DVI
+    ,
     output var logic [2:0] tmds_data_p,
     output var logic [2:0] tmds_data_n,
     output var logic       tmds_clk_p,
     output var logic       tmds_clk_n
 `elsif VGA
+    ,
     output var logic [3:0] red_o,
     output var logic [3:0] green_o,
     output var logic [3:0] blue_o,
@@ -24,93 +28,171 @@ module primer20k_top #(
 `endif
 );
 
-logic       rst;
-logic [5:0] keys;
+import dvi_pkg::W_H_RES;
+import dvi_pkg::W_V_RES;
+import dvi_pkg::W_COLOR;
+
+logic               rst;
+logic [5:0]         keys;
+
+logic               px_clk;
+logic               pll_clk;
+logic               pll_lock;
+
+logic [W_H_RES-1:0] px_x;
+logic [W_V_RES-1:0] px_y;
+logic               in_range;
+
+`ifdef VGA
+logic               hsync;
+logic               vsync;
+logic [2:0]         delay_in;
+logic [2:0]         delay_out;
+logic               hsync_del;
+logic               vsync_del;
+logic               in_range_del;
+`endif
+
+// verilator lint_off UNUSEDSIGNAL
+logic [W_COLOR-1:0] red;
+logic [W_COLOR-1:0] green;
+logic [W_COLOR-1:0] blue;
+// verilator lint_on UNUSEDSIGNAL
 
 assign keys = ~keys_inv_i;
 
-`ifdef DVI
-
-    logic px_clk;
-    logic pixel_clk_div2;
-    logic serial_clk;
-    logic pll_lock;
+// Hides this module from Verilator lint
+`ifdef GOWIN
 
     rPLL #(
+    `ifdef DVI
+        // 252 MHz for DVI serialization
         .FCLKIN    ("27"),
         .IDIV_SEL  (2   ),
         .FBDIV_SEL (27  ),
         .ODIV_SEL  (4   )
-    ) rpll (
-        .CLKIN   (clk       ), // 27 MHZ
-        .CLKOUT  (serial_clk), // 252 MHz
-        .LOCK    (pll_lock  ),
-        .RESET   ('0        ),
-        .RESET_P ('0        ),
-        .CLKFB   ('0        ),
-        .FBDSEL  ('0        ),
-        .IDSEL   ('0        ),
-        .ODSEL   ('0        ),
-        .PSDA    ('0        ),
-        .DUTYDA  ('0        ),
-        .FDLY    ('0        )
-    );
-
-    // Divide by 10 to get 25.2 MHz pixel clock
-
-    CLKDIV2 div_2 (
-        .HCLKIN (serial_clk    ),
-        .CLKOUT (pixel_clk_div2),
-        .RESETN (pll_lock      )
-    );
-
-    CLKDIV #(
-        .DIV_MODE ("5")
-    ) div_5 (
-        .HCLKIN (pixel_clk_div2),
-        .CLKOUT (px_clk        ),
-        .RESETN (pll_lock      )
-    );
-
-`elsif VGA
-
-    logic clk_50_mhz;
-    logic pll_lock;
-    logic px_clk;
-    logic serial_clk;
-
-    rPLL #(
+    `elsif VGA
+        // 50 MHz for VGA (divided by 2 to get 25 MHz pixel clock)
         .FCLKIN    ("27"),
         .IDIV_SEL  (6   ),
         .FBDIV_SEL (12  ),
-        .ODIV_SEL  (16   )
+        .ODIV_SEL  (16  )
+    `endif
     ) rpll (
-        .CLKIN   (clk       ), // 27 MHZ
-        .CLKOUT  (clk_50_mhz), // 50 MHz
-        .LOCK    (pll_lock  ),
-        .RESET   ('0        ),
-        .RESET_P ('0        ),
-        .CLKFB   ('0        ),
-        .FBDSEL  ('0        ),
-        .IDSEL   ('0        ),
-        .ODSEL   ('0        ),
-        .PSDA    ('0        ),
-        .DUTYDA  ('0        ),
-        .FDLY    ('0        )
+        .CLKIN   (clk     ), // 27 MHZ
+        .CLKOUT  (pll_clk ),
+        .LOCK    (pll_lock),
+        .RESET   ('0      ),
+        .RESET_P ('0      ),
+        .CLKFB   ('0      ),
+        .FBDSEL  ('0      ),
+        .IDSEL   ('0      ),
+        .ODSEL   ('0      ),
+        .PSDA    ('0      ),
+        .DUTYDA  ('0      ),
+        .FDLY    ('0      )
     );
 
-    initial px_clk = '0;
+    `ifdef DVI
 
-    always_ff @(posedge clk_50_mhz)
-        px_clk <= !px_clk;
+        logic serial_clk_div2;
+
+        // Divide by 10 to get 25.2 MHz pixel clock
+
+        CLKDIV2 div_2 (
+            .HCLKIN (pll_clk        ),
+            .CLKOUT (serial_clk_div2),
+            .RESETN (pll_lock       )
+        );
+
+        CLKDIV #(
+            .DIV_MODE ("5")
+        ) div_5 (
+            .HCLKIN (serial_clk_div2),
+            .CLKOUT (px_clk         ),
+            .RESETN (pll_lock       )
+        );
+
+        dvi_top dvi_top (
+            .serial_clk  (pll_clk    ),
+            .pixel_clk   (px_clk     ),
+            .rst         (rst        ),
+
+            .red_i       (red        ),
+            .green_i     (green      ),
+            .blue_i      (blue       ),
+
+            .x_o         (px_x       ),
+            .y_o         (px_y       ),
+            .in_range_o  (in_range   ),
+
+            .tmds_data_p (tmds_data_p),
+            .tmds_data_n (tmds_data_n),
+            .tmds_clk_p  (tmds_clk_p ),
+            .tmds_clk_n  (tmds_clk_n )
+        );
+
+    `elsif VGA
+
+        import dvi_pkg::DEL_CYCLES;
+
+
+        initial px_clk = '0;
+
+        // always_ff can't have LHS values that are also driven by other processes
+        // (like initial), see IEEE-1800 2023 9.2.2.4
+        always @(posedge pll_clk)
+            px_clk <= !px_clk;
+
+        // DVI uses the same timings as VGA
+        dvi_sync dvi_sync (
+            .clk_i           (px_clk  ),
+            .rst_i           (rst     ),
+
+            .hsync_o         (hsync   ),
+            .vsync_o         (vsync   ),
+            .pixel_x_o       (px_x    ),
+            .pixel_y_o       (px_y    ),
+            .visible_range_o (in_range)
+        );
+
+        assign delay_in = { hsync, vsync, in_range };
+        assign { hsync_del, vsync_del, in_range_del } = delay_out;
+
+        delay #(
+            .WIDTH    (3         ),
+            .N_CYCLES (DEL_CYCLES)
+        ) delay (
+            .clk    (px_clk   ),
+            .rst    (rst      ),
+            .data_i (delay_in ),
+            .data_o (delay_out)
+        );
+
+        always_ff @(posedge px_clk) begin
+            hsync_o <= hsync_del;
+            vsync_o <= vsync_del;
+
+            red_o   <= '0;
+            green_o <= '0;
+            blue_o  <= '0;
+
+            if (in_range_del) begin
+                red_o   <= red[W_COLOR-1-:4];
+                green_o <= green[W_COLOR-1-:4];
+                blue_o  <= blue[W_COLOR-1-:4];
+            end
+        end
+
+    `endif
 
 `elsif SIMULATION
 
-    bit serial_clk;
-    bit px_clk;
+    initial pll_clk = '0;
+    initial px_clk  = '0;
 
-    always #1  serial_clk = !serial_clk;
-    always #10 px_clk     = !px_clk;
+    always #1  pll_clk = !pll_clk;
+    always #10 px_clk  = !px_clk;
 
 `endif
 
@@ -120,8 +202,7 @@ raycast_top #(
     .MOVEMENT_SPEED (MOVEMENT_SPEED),
     .ROTATION_SPEED (ROTATION_SPEED)
 ) raycast_top (
-    .serial_clk         (serial_clk ),
-    .px_clk             (px_clk     ),
+    .clk                (px_clk     ),
     .rst                (rst        ),
 
     .key_forward_i      (keys[0]    ),
@@ -130,19 +211,14 @@ raycast_top #(
     .key_right_i        (keys[3]    ),
     .key_rotate_left_i  (keys[4]    ),
     .key_rotate_right_i (keys[5]    ),
-`ifdef DVI
-    .tmds_data_p        (tmds_data_p),
-    .tmds_data_n        (tmds_data_n),
-    .tmds_clk_p         (tmds_clk_p ),
-    .tmds_clk_n         (tmds_clk_n )
-`elsif VGA
-    .red_o              (red_o      ),
-    .green_o            (green_o    ),
-    .blue_o             (blue_o     ),
 
-    .hsync_o            (hsync_o    ),
-    .vsync_o            (vsync_o    )
-`endif
+    .px_x_i             (px_x       ),
+    .px_y_i             (px_y       ),
+    .in_range_i         (in_range   ),
+
+    .red_o              (red        ),
+    .green_o            (green      ),
+    .blue_o             (blue       )
 );
 
 endmodule
--- rtl/raycast_top.sv
+++ rtl/raycast_top.sv
@@ -4,44 +4,39 @@
 
 `default_nettype none
 
-module raycast_top #(
+module raycast_top
+    import dvi_pkg::W_H_RES;
+    import dvi_pkg::W_V_RES;
+    import dvi_pkg::W_COLOR;
+#(
     parameter real MOVEMENT_SPEED = 0.8,
     parameter real ROTATION_SPEED = 0.4
 ) (
-    input  var logic       serial_clk,
-    input  var logic       px_clk,
-    input  var logic       rst,
+    input  var logic               clk,
+    input  var logic               rst,
 
     // Key input
-    input  var logic       key_forward_i,
-    input  var logic       key_backward_i,
-    input  var logic       key_left_i,
-    input  var logic       key_right_i,
-    input  var logic       key_rotate_left_i,
-    input  var logic       key_rotate_right_i,
+    input  var logic               key_forward_i,
+    input  var logic               key_backward_i,
+    input  var logic               key_left_i,
+    input  var logic               key_right_i,
+    input  var logic               key_rotate_left_i,
+    input  var logic               key_rotate_right_i,
 
-`ifdef DVI
-    output var logic [2:0] tmds_data_p,
-    output var logic [2:0] tmds_data_n,
-    output var logic       tmds_clk_p,
-    output var logic       tmds_clk_n
-`elsif VGA
-    output var logic [3:0] red_o,
-    output var logic [3:0] green_o,
-    output var logic [3:0] blue_o,
+    // Data for display output
+    input  var logic [W_H_RES-1:0] px_x_i,
+    input  var logic [W_V_RES-1:0] px_y_i,
+    input  var logic               in_range_i,
 
-    output var logic       hsync_o,
-    output var logic       vsync_o
-`endif
+    output var logic [W_COLOR-1:0] red_o,
+    output var logic [W_COLOR-1:0] green_o,
+    output var logic [W_COLOR-1:0] blue_o
 );
 
 import fixp_pkg::*;
 import tex_pkg::W_NUM_TEX;
 import dvi_pkg::FRAME_WIDTH;
 import dvi_pkg::FRAME_HEIGHT;
-import dvi_pkg::W_H_RES;
-import dvi_pkg::W_V_RES;
-import dvi_pkg::W_COLOR;
 
 // ----------------------------------------------------------------------------
 // Local parameters declaration
@@ -74,14 +69,6 @@ logic [W_NUM_TEX-1:0] texture;
 logic                 lookup_render;
 logic                 wall_hit;
 
-logic [W_COLOR-1:0]   red;
-logic [W_COLOR-1:0]   green;
-logic [W_COLOR-1:0]   blue;
-
-logic [W_H_RES-1:0]   px_x;
-logic [W_V_RES-1:0]   px_y;
-logic                 in_range;
-
 logic                 frame_start;
 logic                 frame_done;
 
@@ -110,13 +97,13 @@ ray_fixp_t            plane_y;
 
 assign map_addr = (map_addr_t'(map_y) * map_addr_t'(MAP_SIDE)) + map_addr_t'(map_x);
 
-always_ff @(posedge px_clk)
+always_ff @(posedge clk)
     texture <= map[map_addr];
 
-assign frame_start = (px_x == '0) && (px_y == '0) && in_range;
-assign frame_done  = (px_x == FRAME_WIDTH - 1) && (px_y == FRAME_HEIGHT - 1);
+assign frame_start = (px_x_i == '0) && (px_y_i == '0) && in_range_i;
+assign frame_done  = (px_x_i == FRAME_WIDTH - 1) && (px_y_i == FRAME_HEIGHT - 1);
 
-always_ff @(posedge px_clk)
+always_ff @(posedge clk)
     if (rst)
         lookup_render <= '1;
     else if (frame_start)
@@ -134,16 +121,16 @@ assign wall_hit = texture != '0;
 // ----------------------------------------------------------------------------
 
 render render (
-    .clk            (px_clk      ),
+    .clk            (clk         ),
     .rst            (rst         ),
 
-    .px_x_i         (px_x        ),
-    .px_y_i         (px_y        ),
-    .in_range_i     (in_range    ),
+    .px_x_i         (px_x_i      ),
+    .px_y_i         (px_y_i      ),
+    .in_range_i     (in_range_i  ),
     .new_frame_i    (frame_start ),
-    .red_o          (red         ),
-    .green_o        (green       ),
-    .blue_o         (blue        ),
+    .red_o          (red_o       ),
+    .green_o        (green_o     ),
+    .blue_o         (blue_o      ),
 
     .lookup_map_x_o (render_map_x),
     .lookup_map_y_o (render_map_y),
@@ -163,7 +150,7 @@ controls #(
     .MOVEMENT_SPEED (MOVEMENT_SPEED),
     .ROTATION_SPEED (ROTATION_SPEED)
 ) controls (
-    .clk                (px_clk            ),
+    .clk                (clk               ),
     .rst                (rst               ),
 
     .key_forward_i      (key_forward_i     ),
@@ -188,81 +175,6 @@ controls #(
     .plane_x_o          (plane_x           ),
     .plane_y_o          (plane_y           )
 );
-
-`ifdef DVI
-
-    dvi_top dvi_top (
-        .serial_clk  (serial_clk ),
-        .pixel_clk   (px_clk     ),
-        .rst         (rst        ),
-
-        .red_i       (red        ),
-        .green_i     (green      ),
-        .blue_i      (blue       ),
-
-        .x_o         (px_x       ),
-        .y_o         (px_y       ),
-        .in_range_o  (in_range   ),
-
-        .tmds_data_p (tmds_data_p),
-        .tmds_data_n (tmds_data_n),
-        .tmds_clk_p  (tmds_clk_p ),
-        .tmds_clk_n  (tmds_clk_n )
-    );
-
-`elsif VGA
-
-    import dvi_pkg::DEL_CYCLES;
-
-    logic       hsync;
-    logic       vsync;
-    logic [2:0] delay_in;
-    logic [2:0] delay_out;
-    logic       hsync_del;
-    logic       vsync_del;
-    logic       in_range_del;
-
-    // DVI uses the same timings as VGA
-    dvi_sync dvi_sync (
-        .clk_i           (px_clk  ),
-        .rst_i           (rst     ),
-
-        .hsync_o         (hsync   ),
-        .vsync_o         (vsync   ),
-        .pixel_x_o       (px_x    ),
-        .pixel_y_o       (px_y    ),
-        .visible_range_o (in_range)
-    );
-
-    assign delay_in = { hsync, vsync, in_range };
-    assign { hsync_del, vsync_del, in_range_del } = delay_out;
-
-    delay #(
-        .WIDTH    (3         ),
-        .N_CYCLES (DEL_CYCLES)
-    ) delay (
-        .clk    (px_clk   ),
-        .rst    (rst      ),
-        .data_i (delay_in ),
-        .data_o (delay_out)
-    );
-
-    always_ff @(posedge px_clk) begin
-        hsync_o <= hsync_del;
-        vsync_o <= vsync_del;
-
-        red_o   <= '0;
-        green_o <= '0;
-        blue_o  <= '0;
-
-        if (in_range_del) begin
-            red_o   <= red[W_COLOR-1-:4];
-            green_o <= green[W_COLOR-1-:4];
-            blue_o  <= blue[W_COLOR-1-:4];
-        end
-    end
-
-`endif
 
 endmodule
 
